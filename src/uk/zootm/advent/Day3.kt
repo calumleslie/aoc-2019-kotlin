700dd69deceda25dda6ade6de6179b8482754d24
Calum Leslie <calumleslie@gmail.com>
2019-12-19 12:56:56 +0000

    Day 3: Part 2 solution
    Some refactoring to allow us to reuse the path reading code, and some
    sets are changed to lists to allow us to follow the paths.

--- src/uk/zootm/advent/Day3.kt
+++ src/uk/zootm/advent/Day3.kt
@@ -12,9 +12,8 @@ data class Vector(val x: Int, val y: Int) {
 }
 
 data class Segment(val direction: Direction, val magnitude: Int) {
-    fun pointsFrom(start: Vector): Set<Vector> = (1..magnitude)
+    fun pointsFrom(start: Vector): List<Vector> = (1..magnitude)
         .map { m -> start + direction.vector * m }
-        .toSet()
 
     val vector: Vector = direction.vector * magnitude
 }
@@ -38,8 +37,8 @@ data class Path(val segments: List<Segment>) {
         }
     }
 
-    fun points(): Set<Vector> {
-        val result = HashSet<Vector>()
+    private fun points(): List<Vector> {
+        val result = ArrayList<Vector>()
         var location = ORIGIN
         for (segment in segments) {
             result.addAll(segment.pointsFrom(location))
@@ -48,7 +47,11 @@ data class Path(val segments: List<Segment>) {
         return result
     }
 
-    fun intersect(other: Path) = points().intersect(other.points())
+    fun intersect(other: Path) = points().toSet().intersect(other.points())
+
+    // Note this returns 0 if the point is not in the path. The +1 is to account for the fact that we don't include
+    // origin in the set of points
+    fun stepsTo(vector: Vector) = points().indexOf(vector) + 1
 }
 
 val ORIGIN = Vector(0, 0)
@@ -59,10 +62,6 @@ enum class Direction(x: Int, y: Int) {
     LEFT(-1, 0),
     RIGHT(1, 0);
 
-    companion object Parser {
-
-    }
-
     val vector = Vector(x, y)
     operator fun times(magnitude: Int) = vector * magnitude
 }
@@ -72,12 +71,29 @@ fun closestToOrigin(points: Set<Vector>) = points.minBy { it.manhattanTo(ORIGIN)
 fun distanceToClosestIntersection(path1: Path, path2: Path): Int? =
     closestToOrigin(path1.intersect(path2))?.manhattanTo(ORIGIN)
 
+fun stepsToLeastCostlyIntersection(path1: Path, path2: Path): Int? {
+    val intersections = path1.intersect(path2)
+    return intersections
+        .map { path1.stepsTo(it) + path2.stepsTo(it) }
+        .min()
+}
+
 fun main() {
     example1()
     example2()
+    withInputPaths { path1, path2 -> println(distanceToClosestIntersection(path1, path2)) }
 
-    FileInputStream("inputs/day3.1.txt").bufferedReader().use {
-        println(distanceToClosestIntersection(Path.parse(it.readLine()), Path.parse(it.readLine())))
+    example3()
+    example4()
+    withInputPaths { path1, path2 -> println(stepsToLeastCostlyIntersection(path1, path2)) }
+}
+
+fun <T> withInputPaths(function: (Path, Path) -> T): T {
+    return FileInputStream("inputs/day3.1.txt").bufferedReader().use {
+        function.invoke(
+            Path.parse(it.readLine()),
+            Path.parse(it.readLine())
+        )
     }
 }
 
@@ -93,6 +109,24 @@ fun example1() {
 fun example2() {
     assertEquals(
         135, distanceToClosestIntersection(
+            Path.parse("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+            Path.parse("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+        )
+    )
+}
+
+fun example3() {
+    assertEquals(
+        610, stepsToLeastCostlyIntersection(
+            Path.parse("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+            Path.parse("U62,R66,U55,R34,D71,R55,D58,R83")
+        )
+    )
+}
+
+fun example4() {
+    assertEquals(
+        410, stepsToLeastCostlyIntersection(
             Path.parse("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
             Path.parse("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
         )
