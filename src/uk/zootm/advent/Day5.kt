7a7f7347facd2b123e7de8ff7118c53c614b1dc3
Calum Leslie <calumleslie@gmail.com>
2020-01-12 17:47:58 +0000

    Day 5: Part 2 solution
    As before the changes to the intcode interpreter were all that was
    required.

--- src/uk/zootm/advent/Day5.kt
+++ src/uk/zootm/advent/Day5.kt
@@ -8,8 +8,16 @@ import java.io.File
 fun main() {
     val base = Memory.fromFile(File("inputs/day5.1.txt"))
 
-    base.makeComputer(
+    println("# PART 1")
+    base.copy().makeComputer(
         input = Input.of(1),
+        output = PrintingOutput()
+    ).execute()
+
+    println()
+    println("# PART 2")
+    base.copy().makeComputer(
+        input = Input.of(5),
         output = PrintingOutput()
     ).execute()
 }
